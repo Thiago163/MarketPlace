7d4cfbf69f5119562714b802a2dc6557b9275e14
Thiago_Cruz <101190863+mistickesterio@users.noreply.github.com>
2022-11-24 11:00:16 -0300

    third commit
    ainda não completo

--- banco de dados projeto de ferias.sql
+++ banco de dados projeto de ferias.sql
@@ -3,58 +3,72 @@ CREATE DATABASE MarketPlace;
 USE MarketPlace;
 
 CREATE TABLE Lojista(
-    ID INT NOT NULL PRIMARY KEY AUTO_increment,
+	ID INT NOT NULL PRIMARY KEY AUTO_increment,
     Nome VARCHAR(255)
 );
 
 CREATE TABLE Produto(
-    ID INT NOT NULL primary key auto_increment,
+	ID INT NOT NULL primary key auto_increment,
     Nome varchar(255),
     Preco INT,
     Descricao LONGTEXT,
-    Lojista INT NOT NULL,
+	Lojista INT NOT NULL,
     foreign key (lojista) references lojista(id)
 );
 
 CREATE TABLE Categoria(
-    ID INT NOT NULL primary key auto_increment,
+	ID INT NOT NULL primary key auto_increment,
     Nome VARCHAR(255)
 );
 
 CREATE TABLE CategoriasProduto(
-    Produto INT NOT NULL,
-    Categoria INT NOT NULL,
+	Produto INT NOT NULL,
+	Categoria INT NOT NULL,
     foreign key (Categoria) references Categoria(id),
     foreign key (Produto) references Produto(id)
 );
     
     insert into Lojista(nome) values
     ("Vendedor falido"),
-    ("Vendedor de sucesso"),
-    ("Oque estou fazendo aqui?"),
-    ("Danoninho semi gratis"),
-    ("Vendedor de danone");
+	("Vendedor de sucesso"),
+	("Oque estou fazendo aqui?"),
+	("Danoninho semi gratis"),
+    ("Vendedor de todinho"),
+    ("Ivete"),
+	("Vendedor safado"),
+	("Oque era para eu fazer aqui?"),
+	("Danoninho semi pago"),
+    ("Anita");
     
     insert into Produto values
     (default, "Computador I9", 500.00, "Computador muito bom", 1),
     (default, "Computador celeron", 100.00, "Computador muito bom", 2),
     (default, "Danoninho", 5.00, "muito bom", 3),
     (default, "Chocolate", 10.00, "muito bom", 4),
+    (default, "Vassoura", 20.00, "ruim", 5),
+    (default, "Computador I7", 500.00, "Computador muito bom", 1),
+    (default, "Computador i5", 100.00, "Computador muito bom", 2),
+    (default, "Todinho", 5.00, "muito bom", 3),
+    (default, "Vestido", 10.00, "muito bom", 4),
     (default, "Vassoura", 20.00, "ruim", 5);
     
     insert into Categoria(nome) values
     ("Alimentos"),
-    ("Eletronicos"),
-    ("Serviços"),
-    ("Produtos geral");
+	("Eletronicos"),
+	("Serviços"),
+	("Veiculos"),
+    ("Limpeza"),
+	("Moda"),
+	("Usados"),
+	("Produtos geral");
     
     insert into CategoriasProduto values
     (1, 2),
-    (1, 1),
-    (2, 3),
-    (2, 4),
-    (3, 1),
-    (3, 1);
+	(1, 1),
+	(2, 3),
+	(2, 4),
+	(3, 1),
+	(3, 1);
     
     select * from Lojista, Produto, Categoria;
     select * from Lojista, Categoria, Produto where preco > 100.00;
