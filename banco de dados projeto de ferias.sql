fa4c5b0fbba49131efb064bdea126fee63c67e28
Thiago_Cruz <101190863+mistickesterio@users.noreply.github.com>
2022-11-24 11:03:23 -0300

    forth commit

--- banco de dados projeto de ferias.sql
+++ banco de dados projeto de ferias.sql
@@ -3,41 +3,41 @@ CREATE DATABASE MarketPlace;
 USE MarketPlace;
 
 CREATE TABLE Lojista(
-	ID INT NOT NULL PRIMARY KEY AUTO_increment,
+    ID INT NOT NULL PRIMARY KEY AUTO_increment,
     Nome VARCHAR(255)
 );
 
 CREATE TABLE Produto(
-	ID INT NOT NULL primary key auto_increment,
+    ID INT NOT NULL primary key auto_increment,
     Nome varchar(255),
     Preco INT,
     Descricao LONGTEXT,
-	Lojista INT NOT NULL,
+    Lojista INT NOT NULL,
     foreign key (lojista) references lojista(id)
 );
 
 CREATE TABLE Categoria(
-	ID INT NOT NULL primary key auto_increment,
+    ID INT NOT NULL primary key auto_increment,
     Nome VARCHAR(255)
 );
 
 CREATE TABLE CategoriasProduto(
-	Produto INT NOT NULL,
-	Categoria INT NOT NULL,
+    Produto INT NOT NULL,
+    Categoria INT NOT NULL,
     foreign key (Categoria) references Categoria(id),
     foreign key (Produto) references Produto(id)
 );
     
     insert into Lojista(nome) values
     ("Vendedor falido"),
-	("Vendedor de sucesso"),
-	("Oque estou fazendo aqui?"),
-	("Danoninho semi gratis"),
+    ("Vendedor de sucesso"),
+    ("Oque estou fazendo aqui?"),
+    ("Danoninho semi gratis"),
     ("Vendedor de todinho"),
     ("Ivete"),
-	("Vendedor safado"),
-	("Oque era para eu fazer aqui?"),
-	("Danoninho semi pago"),
+    ("Vendedor safado"),
+    ("Oque era para eu fazer aqui?"),
+    ("Danoninho semi pago"),
     ("Anita");
     
     insert into Produto values
@@ -54,21 +54,21 @@ CREATE TABLE CategoriasProduto(
     
     insert into Categoria(nome) values
     ("Alimentos"),
-	("Eletronicos"),
-	("Serviços"),
-	("Veiculos"),
+    ("Eletronicos"),
+    ("Serviços"),
+    ("Veiculos"),
     ("Limpeza"),
-	("Moda"),
-	("Usados"),
-	("Produtos geral");
+    ("Moda"),
+    ("Usados"),
+    ("Produtos geral");
     
     insert into CategoriasProduto values
     (1, 2),
-	(1, 1),
-	(2, 3),
-	(2, 4),
-	(3, 1),
-	(3, 1);
+    (1, 1),
+    (2, 3),
+    (2, 4),
+    (3, 1),
+    (3, 1);
     
     select * from Lojista, Produto, Categoria;
     select * from Lojista, Categoria, Produto where preco > 100.00;
